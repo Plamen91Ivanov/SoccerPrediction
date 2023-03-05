59a0864d9e7c2e3cf12fae4d357abdb8cf970b99
PlamenIvanov <plamen91ivanov@abv.bg>
2023-03-05 10:30:56 +0200

    live matches

---
+++ Soccer2/Controllers/MatchController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Soccer2.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Soccer2.Controllers
+{
+    public class MatchController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public MatchController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Matches()
+        {
+            var matches = this.db.Games.ToList();
+
+            return View(matches);
+        }
+    }
+}
